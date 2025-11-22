c8e4dacb72c8b47ba87188fcfdc99117fe686d7b
indextrown <indextrown@gmail.com>
2025-11-23 03:56:25 +0900

    chore: 프로젝트 세팅

---
+++ src/main/java/com/indextrown/serverbaseapi/ServerBaseApiApplication.java
@@ -0,0 +1,13 @@
+package com.indextrown.serverbaseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerBaseApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerBaseApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/indextrown/serverbaseapi/ServerBaseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indextrown.serverbaseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerBaseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
